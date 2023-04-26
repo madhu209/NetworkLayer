f8d6cbeb296be94ea75108417b2fdda18c9410c1
madhu209 <44475717+madhu209@users.noreply.github.com>
2023-04-26 10:10:52 +0530

    header refactor

--- NetworkLayer/Network/Network/NetworkController.swift
+++ NetworkLayer/Network/Network/NetworkController.swift
@@ -13,7 +13,7 @@ open class NetworkController {
 
     }
 
-    public func fetchResponse<ModelType: Decodable>(url: URL, queryItems: [URLQueryItem]?, headers: [String: String] = Header().defaultHeaders, body: JSONBody, method: URLRequest.HTTPMethod, completion: @escaping (Result<ModelType, Error>) -> Void) {
+    public func fetchResponse<ModelType: Decodable>(url: URL, queryItems: [URLQueryItem]?, headers: [String: String] = Header().headers, body: JSONBody, method: URLRequest.HTTPMethod, completion: @escaping (Result<ModelType, Error>) -> Void) {
 
         guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return }
         if urlComponents.queryItems == nil {
@@ -52,42 +52,43 @@ open class NetworkController {
         }.resume()
     }
 
-//    public func fetchResponse<ModelType: Decodable>(url: URL, queryItems: [URLQueryItem]?, headers: [String: Any] = Headers().defaultHeaders, body: JSONBody, method: URLRequest.HTTPMethod, completion: @escaping (Result<ModelType, Error>) -> Void) {
-//
-//        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return }
-//        if urlComponents.queryItems == nil {
-//            urlComponents.queryItems = queryItems
-//        } else if let queryItems {
-//            urlComponents.queryItems?.insert(contentsOf: queryItems, at: 0)
-//        }
-//
-//        var urlRequest = URLRequest(url: urlComponents.url!)
-//        urlRequest.httpMethod = method.rawValue
-//
-//        for (key, value) in headers {
-//            urlRequest.addValue(value as! String, forHTTPHeaderField: key)
-//        }
-//
-//        if method == .post {
-//            urlRequest.httpBody = try? body.encode()
-//        }
-//
-//        URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
-//            if let data = data {
-//
-//                debugPrint(String(data: data, encoding: .utf8)!)
-//                let jsonDecoder = JSONDecoder()
-//
-//                do {
-//                    let result = try jsonDecoder.decode(ModelType.self, from: data)
-//                    completion(.success(result))
-//                } catch {
-//                    debugPrint(error.localizedDescription)
-//                    completion(.failure(error))
-//                }
-//            }
-//        }.resume()
-//
-//    }
+    /*
+    public func fetchResponse<ModelType: Decodable>(url: URL, queryItems: [URLQueryItem]?, headers: [String: Any] = Headers().defaultHeaders, body: JSONBody, method: URLRequest.HTTPMethod, completion: @escaping (Result<ModelType, Error>) -> Void) {
+
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return }
+        if urlComponents.queryItems == nil {
+            urlComponents.queryItems = queryItems
+        } else if let queryItems {
+            urlComponents.queryItems?.insert(contentsOf: queryItems, at: 0)
+        }
+
+        var urlRequest = URLRequest(url: urlComponents.url!)
+        urlRequest.httpMethod = method.rawValue
+
+        for (key, value) in headers {
+            urlRequest.addValue(value as! String, forHTTPHeaderField: key)
+        }
+
+        if method == .post {
+            urlRequest.httpBody = try? body.encode()
+        }
+
+        URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
+            if let data = data {
+
+                debugPrint(String(data: data, encoding: .utf8)!)
+                let jsonDecoder = JSONDecoder()
+
+                do {
+                    let result = try jsonDecoder.decode(ModelType.self, from: data)
+                    completion(.success(result))
+                } catch {
+                    debugPrint(error.localizedDescription)
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+
+    }*/
 
 }
--- NetworkLayer/Network/ProtocolRequest/HTTPHeaders.swift
+++ NetworkLayer/Network/ProtocolRequest/HTTPHeaders.swift
@@ -8,17 +8,14 @@
 import Foundation
 
 protocol HTTPHeaders {
-    var defaultHeaders: [String: String] { get set }
+    var defaultHeaders: [String: String] { get }
+    var headers: [String: String] { get set }
     func setHeader(value: String, key: String)
 }
 
 public class Header: HTTPHeaders {
 
     public init() {}
-
-    public func setHeader(value: String, key: String) {
-        self.defaultHeaders[key] = value
-    }
 
     public var defaultHeaders: [String : String] = [HeaderFieldKeys.contentType: HeaderFieldValues.contentType]
 
@@ -27,7 +24,12 @@ public class Header: HTTPHeaders {
             debugPrint("custom headers is: \(newHeaders)")
         } didSet {
             headers = headers.merging(defaultHeaders) { (current, _) in current }
+            debugPrint("headers is: \(headers)")
         }
+    }
+
+    public func setHeader(value: String, key: String) {
+        self.headers[key] = value
     }
 
 }
