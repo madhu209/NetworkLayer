e2526601a50d3ea3af994e75bdb964e5455a04a7
madhu209 <44475717+madhu209@users.noreply.github.com>
2023-05-15 17:42:36 +0530

    async await

--- NetworkLayer/Network/Network/NetworkController.swift
+++ NetworkLayer/Network/Network/NetworkController.swift
@@ -51,9 +51,8 @@ open class NetworkController {
             }
         }.resume()
     }
-
-    /*
-    public func fetchResponse<ModelType: Decodable>(url: URL, queryItems: [URLQueryItem]?, headers: [String: Any] = Headers().defaultHeaders, body: JSONBody, method: URLRequest.HTTPMethod, completion: @escaping (Result<ModelType, Error>) -> Void) {
+/*
+    public func fetchResponse<ModelType: Decodable>(url: URL, queryItems: [URLQueryItem]?, headers: [String: String] = Header().headers, body: JSONBody = JSONBody(Empty()), method: URLRequest.HTTPMethod, completion: @escaping (Result<ModelType, Error>) -> Void) {
 
         guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return }
         if urlComponents.queryItems == nil {
@@ -62,33 +61,42 @@ open class NetworkController {
             urlComponents.queryItems?.insert(contentsOf: queryItems, at: 0)
         }
 
-        var urlRequest = URLRequest(url: urlComponents.url!)
-        urlRequest.httpMethod = method.rawValue
-
-        for (key, value) in headers {
-            urlRequest.addValue(value as! String, forHTTPHeaderField: key)
+        let urlHTTPRequest = URLHTTPRequest(urlComponents: urlComponents, method: method, headers: headers)
+        Task {
+            do {
+                let modelType = try await performRequest(modelType: ModelType.self, request: urlHTTPRequest, body: body)
+                completion(.success(modelType))
+            } catch {
+                completion(.failure(error))
+            }
         }
+    }
 
-        if method == .post {
+    func performRequest<ModelType: Decodable>(modelType: ModelType.Type, request: any HTTPRequest, body: JSONBody) async throws -> ModelType {
+
+        var urlRequest = URLRequest(url: request.urlComponents.url!)
+        urlRequest.httpMethod = request.method.rawValue
+        urlRequest.allHTTPHeaderFields = request.headers
+        if request.method == .post {
             urlRequest.httpBody = try? body.encode()
         }
 
-        URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
-            if let data = data {
+        return try await withCheckedThrowingContinuation({ continuation in
+            URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
+                if let data = data {
+                    debugPrint(String(data: data, encoding: .utf8)!)
+                    let jsonDecoder = JSONDecoder()
 
-                debugPrint(String(data: data, encoding: .utf8)!)
-                let jsonDecoder = JSONDecoder()
-
-                do {
-                    let result = try jsonDecoder.decode(ModelType.self, from: data)
-                    completion(.success(result))
-                } catch {
-                    debugPrint(error.localizedDescription)
-                    completion(.failure(error))
+                    do {
+                        let result = try jsonDecoder.decode(ModelType.self, from: data)
+                        continuation.resume(returning: result)
+                    } catch {
+                        debugPrint(error.localizedDescription)
+                        continuation.resume(throwing: error)
+                    }
                 }
-            }
-        }.resume()
-
+            }.resume()
+        })
     }*/
 
 }
